40a11cad7017c5a298e23016c0934501c2ad7397
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-07-05 22:24:25 +0200

    add config.env

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
-const PORT = 3000;
 const mongoose = require("mongoose");
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 
 const vehicleRouter = require("./routes/vehicleRoutes");
 
 const app = express();
 
-const myLogg = (req, res, next) => {
-  // console.log("hello middleware!");
-  next();
-};
+// console.log(process.env.PORT);
 
 //Parsing requests
 app.use(express.json());
-app.use(myLogg);
 
 app.use("/vehicles", vehicleRouter);
 
-mongoose.connect(
-  "mongodb+srv://nastaran1366:american2@cluster0.dtjckyp.mongodb.net/test",
-  {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  }
-);
+  })
+  .then(() => console.log("DB connection successful!"));
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//   console.log("Connected successfully");
+// });
 
-app.listen(PORT, () => {
-  console.log(`I am listening on port: ${PORT}`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`I am listening on port: ${port}`);
 });
